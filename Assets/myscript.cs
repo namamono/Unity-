cfc26b84da045ae4635335d4447a7e9059f9edb3
namamono <mangan422@gmail.com>
2020-09-30 16:43:49 +0900

    初コミット

---
+++ Assets/myscript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class myscript : MonoBehaviour
+{
+    Texture texture1;
+    Renderer renderer;
+    Color color1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       texture1 = (Texture)Resources.Load("QS-GRASS-1.1");
+       renderer = GetComponent<Renderer>();
+       color1 = renderer.material.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(1f,1f,0.1f);
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            renderer.material.mainTexture=texture1;
+            renderer.material.color=Color.white;
+        }
+        if(Input.GetKeyUp(KeyCode.Space))
+        {
+            renderer.material.mainTexture=null;
+            renderer.material.color = color1;
+        }
+    }
+}
---
+++ Assets/rigidscript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rigidscript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       transform.Rotate(1f,1f,0.1f);
+       if(Input.GetKeyDown(KeyCode.Space))
+       {
+           GetComponent<Renderer>().enabled=false;
+       }
+       if(Input.GetKeyUp(KeyCode.Space))
+       {
+           GetComponent<Renderer>().enabled=true;
+       }
+    }
+}
