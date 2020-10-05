38c90df3dad14882a2242ec740651681250bfc97
namamono <mangan422@gmail.com>
2020-10-05 16:57:41 +0900

    3-9

---
+++ Assets/CubePlayableAsset.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+[System.Serializable]
+public class CubePlayableAsset : PlayableAsset
+{
+    // Factory method that generates a playable based on this asset
+    public override Playable CreatePlayable(PlayableGraph graph, GameObject go)
+    {
+        CubePlayableBehaviour behaviour = new CubePlayableBehaviour()
+        behaviour.cubeObject=go;
+        ScriptPlayable<CubePlayableBehaviour> playable = ScriptPlayable<CubePlayableBehaviour>.Create(graph,behaviour);
+        return Playable;
+    }
+}
